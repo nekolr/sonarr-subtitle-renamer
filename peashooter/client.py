5c44bae32400aeb774717fd5b25aa72808a86712
nekolr <excalibll@163.com>
2026-01-11 19:05:02 +0800

    update peashooter api

--- peashooter/client.py
+++ peashooter/client.py
@@ -6,13 +6,13 @@ from peashooter.api import PeashooterApi
 class PeashooterClient(PeashooterApi):
 
     def get_series_list(self):
-        get_series_list_uri = '/api/sonarr/series'
+        get_series_name_list_uri = '/api/sonarr/seriesNameList'
         params = {"apiKey": self.api_key}
-        resp = requests.get(self.base_url + get_series_list_uri, params)
+        resp = requests.get(self.base_url + get_series_name_list_uri, params)
         return json.loads(resp.text)['data'] if resp.status_code == 200 else []
 
     def refresh_series_list(self):
-        refresh_series_list_uri = '/api/sonarr/refresh-series'
+        refresh_series_name_list_uri = '/api/sonarr/refreshSeriesNameList'
         params = {"apiKey": self.api_key}
-        resp = requests.get(self.base_url + refresh_series_list_uri, params)
+        resp = requests.get(self.base_url + refresh_series_name_list_uri, params)
         return json.loads(resp.text)['data'] if resp.status_code == 200 else []
